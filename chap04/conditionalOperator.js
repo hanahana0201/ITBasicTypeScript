fce3f82b4b9fd9dc4a54b30cfbb7f60ccf8f62c0
Masatoshi Hanai <hanahana0201@gmail.com>
2022-05-01 12:02:31 +0900

    Finish 4-4

---
+++ chap04/conditionalOperator.js
@@ -0,0 +1,11 @@
+"use strict";
+exports.__esModule = true;
+var num = Math.round(Math.random() * 100);
+console.log("num\u306E\u5024: ".concat(num));
+if (num % 2 == 0 && num % 3 == 0) {
+    console.log("2の倍数であり、しかも3の倍数");
+}
+else {
+    console.log("それ以外");
+}
+console.log("処理終了");
